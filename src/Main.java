1f6516b4f2e8f55db3f84f50827e3b30cf8f1129
ConKerazoE2906 <pablo.erazo01@epn.edu.ec>
2023-12-19 09:55:12 -0500

    second

---
+++ src/Main.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    /*public static void main(String[] args) {
+        System.out.println("Ejercicios");
+        String[] marcas={"Mercedes","Nissan","Kia","Great Wall", "Toyota","Suzuki","Foton", "Fiat","Lifan", "Mazda"};
+        int[] pares={2,4,6,8,10,12,14,16,18,20,22,24,26,28,30};
+        for (int i=0;i<3;i++){
+            System.out.println(marcas[i]);
+        }
+        marcas[0]="Toyota";
+        System.out.println("valor nuevo");
+
+
+
+        System.out.println(marcas.length);
+
+        int[][] numeros={{1,2,3,4},{5,6,7,8}};
+        //System.out.println( numeros.length;//imprime 2)
+        System.out.println(numeros[1].length);
+       //System.out.println(nuemeros[0][0]); inprime 1
+       //System.out.println(numeros[0]);//direccion de memoria
+
+
+    }*/
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        /*......................................................................................
+
+
+        float[] POO =new float[0];
+        float[] ESTADISTICA=new float[0];
+        float[] GEOMETRIA=new float[0];
+
+        for(int i=0;i<1;i++){
+            System.out.println("Ingresar la calificacion de POO: ");POO[i]=entrada.nextFloat();
+            System.out.println("Ingrese la calificacion de ESTADISTICA: ");ESTADISTICA[i]=entrada.nextFloat();
+            System.out.println("Ingresas la calificacion de GEOMETRIA: ");GEOMETRIA[i]=entrada.nextFloat();
+
+        }
+        for(int i=0;i<1;i++){
+            System.out.println("La calificacion de POO es: "+POO[i]);
+            System.out.println("La calificacion de ESTADISTICA es: "+ESTADISTICA[i]);
+            System.out.println("la calificacion de GEOMETRIA es: "+GEOMETRIA[i]);
+        }*/
+        //................................................
+        /*............................. de pequeño a grande
+        int entero=5;
+        long miLong= entero;
+        double miDouble =miLong;
+
+
+        System.out.println(entero);
+        System.out.println(miLong);
+        System.out.println(miDouble);
+
+        }*/
+        //...............................................
+        //..................... de grande a pequeño
+        // double-float-long-int-char-short-byte
+
+        /*.........
+        double midoble=9.46d;
+
+        int mientero=(int)midoble;
+
+        System.out.println(mientero);
+        System.out.println(midoble);
+        }*/
+        /*
+        double midouble= 20.52d;
+        long milong=(long) midouble;
+        float mifloat=(float) milong;
+        char michar=(char) mifloat;
+        int mientero=(int) michar;
+        byte mibyte=(byte) mientero;
+
+        System.out.println(midouble);
+        System.out.println(milong);
+        System.out.println(michar);
+        System.out.println(mientero);
+        System.out.println(mibyte);
+
+
+    }*/
+        /*
+        String mystring = "123456789";
+        int numeroconvrtido = Integer.valueOf(mystring);
+        float numerodos = Float.valueOf(mystring);
+        System.out.println(numeroconvrtido + 1);
+        System.out.println(numerodos);
+    }*/
+        /*
+        try{
+            //esto es lo que va a intentar
+            int numero=Integer.parseInt("Cuarenta");
+            System.out.println(numero);
+        }
+
+        catch (Exception e){
+                //Esto es lo que va hacer si se ejecuta el error
+            System.out.println("El usuario debe ingresar valores numericos");
+        }
+        finally {
+            //este codigo se ejecuta si sale mal la operaxion
+            System.out.println("muchas gracias oir usasr el programa ");
+        }*/
+
+        /*
+        float kilos;
+        float altura;
+        try {
+            int kilos = Integer.parseInt("");
+        }
+        */
+        List<String> lista=new ArrayList<>()=ArrayList.asList(marcas);
+    }
+
+
+
+
+}
